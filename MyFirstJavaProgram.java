d96c1914172a4ecb7690976b8c3671e280f8add8
pareshvaghasiya <pareshvaghasiyauk@gmail.com>
2022-04-02 12:02:21 +0100

    This is my first java programme

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+package com.learning.java;
+
+import static com.manualtesting.java.ManualTestingTopic.topicindex;
+import static com.manualtesting.java.Namedata.Namedatanew;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        Namedatanew();
+        topicindex();
+    }
+}
